95fee56a47c6bd921063bf701c3ebeb76cbbe10a
Toby Mackenzie <tobym@tobymackenzie.com>
2012-04-25 22:12:12 -0400

    app/init: changed from symlink to file, now include the other file, will allow custom init stuff to be done easily

--- app/init.php
+++ app/init.php
@@ -1,1 +1,6 @@
-../vendor/_bundles/TJM/Shared/app/init.php
+<?php
+if(!isset($tjmGlobals)) $tjmGlobals = Array();
+if(!array_key_exists('pathApp', $tjmGlobals)){
+	$tjmGlobals['pathApp'] = __DIR__;
+}
+include("../vendor/_bundles/TJM/Shared/app/init.php");
