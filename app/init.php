5d1804bb199ef8e61d23e91fea6e8c0cc04fc464
Toby Mackenzie <tobym@tobymackenzie.com>
2012-04-28 21:37:22 -0400

    app/console: now php file with include for shared version to allow app directory to be more easily set app/init: make sure '__DIR__' on include so it will work for everything

--- app/init.php
+++ app/init.php
@@ -3,4 +3,4 @@ if(!isset($tjmGlobals)) $tjmGlobals = Array();
 if(!array_key_exists('pathApp', $tjmGlobals)){
 	$tjmGlobals['pathApp'] = __DIR__;
 }
-include("../vendor/bundles/TJM/Shared/app/init.php");
+include_once(__DIR__."/../vendor/bundles/TJM/Shared/app/init.php");
