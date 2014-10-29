9595f87d3b4d43ba80b1914a7ec3eb44f3f54ea3
Toby Mackenzie <public@tobymackenzie.com>
2014-10-28 23:57:02 -0400

    web/app: move umask stuff from 'app_dev' to 'app'

--- web/app.php
+++ web/app.php
@@ -3,6 +3,10 @@
 use TJM\Bundle\StandardEditionBundle\Component\App\WebApp;
 use TJM\Bundle\StandardEditionBundle\Component\Config;
 
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+//umask(0000);
+
 require_once __DIR__ . '/../app/init.php';
 
 if(Config::getEnvironment() === 'dev'){
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,10 +1,6 @@
 <?php
 use TJM\Bundle\StandardEditionBundle\Component\Config;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-
 require_once __DIR__ . '/../app/init.php';
 Config::setEnvironment('dev');
 require __DIR__ . '/app.php';
