aec20b75e0f498860b1e3ad0a974a768939270e4
Toby Mackenzie <public@tobymackenzie.com>
2015-03-28 15:27:29 -0400

    Redirect trailing slashes to no trailing slashes if other route not found.  split AppBundle routing into 'routing.pre' and 'routing.post' to ensure some routes are after everything

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use TJM\Bundle\BaseBundle\Controller\Controller;
 
 class DefaultController extends Controller{
@@ -10,6 +11,17 @@ class DefaultController extends Controller{
 			'AppBundle:default:index.html.twig'
 			,$data
 		);
+	}
+	/*
+	Action: removeTrailingSlashAction
+	Redirect any otherwise unmatched routes with a trailing slash to the same route without the trailing slash.
+	-@ http://symfony.com/doc/current/cookbook/routing/redirect_trailing_slash.html
+	*/
+	public function removeTrailingSlashAction(Request $request){
+		$pathInfo = $request->getPathInfo();
+		$requestUri = $request->getRequestUri();
+		$url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+		return $this->redirect($url, ($this->get('kernel')->getEnvironment() === 'dev') ? 302 : 302);
 	}
 	public function simplePageAction($name){
 		return $this->renderPage(
