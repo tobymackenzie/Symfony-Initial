9f071e3f5adb6ab34dec872f865b58183f660eb8
Toby Mackenzie <public@tobymackenzie.com>
2015-05-23 18:18:04 -0400

    autoload: set project path and derive other paths from it

--- app/autoload.php
+++ app/autoload.php
@@ -4,7 +4,7 @@ namespace TJM\Bundle\StandardEditionBundle\Component\App;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-define(__NAMESPACE__ . '\APP_DIR', realpath(__DIR__));
+define(__NAMESPACE__ . '\PROJECT_DIR', realpath(__DIR__ . '/..'));
 define(__NAMESPACE__ . '\VENDOR_DIR', realpath(__DIR__ . '/../vendor'));
 
 /**
@@ -22,9 +22,10 @@ App::getSingleton(Array(
 		//--cli paths
 		'PHPCLI'=> '/usr/bin/php'
 		//--symfony paths
-		,'app'=> constant(__NAMESPACE__ . '\APP_DIR')
-		,'src'=> constant(__NAMESPACE__ . '\APP_DIR') . '/../src'
-		,'var'=> constant(__NAMESPACE__ . '\APP_DIR') . '/../var'
+		,'app'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/app'
+		,'project'=> constant(__NAMESPACE__ . '\PROJECT_DIR')
+		,'src'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/src'
+		,'var'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/var'
 		,'vendor'=> constant(__NAMESPACE__ . '\VENDOR_DIR')
 	)
 	//--See [Symfony's docs on "Setting up Permissions"](http://symfony.com/doc/current/book/installation.html#configuration-and-setup).  Uncomment the following line if you can't use ACL's
